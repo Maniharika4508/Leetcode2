c00181ae3d8dabebc0266971a7fe5e40cc26129c
Maniharika4508 <maniharika945@gmail.com>
2025-07-23 04:54:05 +0000

    Sync LeetCode submission Runtime - 3 ms (58.55%), Memory - 18 MB (100.00%)

---
+++ 3172-divisible-and-non-divisible-sums-difference/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def differenceOfSums(self, n: int, m: int) -> int:
+     sum1=0
+     sum2=0
+     for i in range(1,n+1):
+         if i%m!=0:
+           sum1+=i
+     for j in range (1,n+1):
+         if j%m==0:
+            sum2+=j
+     return sum1-sum2        
+         
