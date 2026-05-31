754677694640f3026ade48ec8e669feefcdd8675
Maniharika4508 <maniharika945@gmail.com>
2026-05-31 12:01:38 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.1 MB (98.80%)

--- 0263-ugly-number/solution.py
+++ 0263-ugly-number/solution.py
@@ -1,8 +1,11 @@
 class Solution:
     def isUgly(self, n: int) -> bool:
-        if n==0:
-            return False
-        for prime in 2,3,5:
-            while n%prime==0:
-                n//=prime
-        return n==1
+     if n <= 0:
+        return False
+     for i in [2, 3, 5]:
+        while n % i == 0:
+            n //= i
+     if n == 1:
+        return True
+     else:
+        return False
