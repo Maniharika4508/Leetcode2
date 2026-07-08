e28b2f71fe84f6d3bda5b45337248d38de8038e0
Maniharika4508 <maniharika945@gmail.com>
2026-07-08 05:17:12 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.2 MB (53.88%)

---
+++ 1236-n-th-tribonacci-number/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        a,b,c=0,1,1
+        if n==0:
+            return 0
+        elif n==1:
+            return 1
+        elif n==2:
+            return 1
+        for i in range(3,n+1):
+                 a,b,c=b,c,a+b+c
+        return c
+               
